f301c337c6837cac701c8109127c9221922f8b5a
SevilayOnogul <sevilayonogul34@hotmail.com>
2025-12-19 00:03:08 +0300

    Admin layout yapısı tamamlandı ve Area kullanımı eklendi

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    public class MovieController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult MovieList()
+        {
+            return View();
+        }
+    }
+}
